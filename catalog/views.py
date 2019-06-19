132a0727db5071a6e1f75e1c8a28906576760611
mattspeidel <mattspeidel@gmail.com>
2019-06-19 12:42:18 -0400

    add registration/login.html, add session info

--- catalog/views.py
+++ catalog/views.py
@@ -17,6 +17,10 @@ def index(request):
     num_authors = Author.objects.count()
     num_genres = Genre.objects.count()
     num_books_w_game = Book.objects.filter(title__icontains="game").count()
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
     
     context = {
         'num_books': num_books,
@@ -25,6 +29,7 @@ def index(request):
         'num_authors': num_authors,
         'num_genres': num_genres,
         'num_books_w_game': num_books_w_game,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
